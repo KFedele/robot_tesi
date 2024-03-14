e7794c923407c793e5c4eda894195844ad4118d3
KFedele <48526171+KFedele@users.noreply.github.com>
2024-03-14 10:44:19 +0100

    Update complete_server.py

--- TaskSchedulingServer/complete_server.py
+++ TaskSchedulingServer/complete_server.py
@@ -69,6 +69,7 @@ elementi_posizioni.sort(key=lambda x: x[1])
 
 # Estrai gli elementi ordinati
 c = [elemento[0] for elemento in elementi_posizioni]
+c = c / np.sum(c)
 
 ########Definizione parametri griglia intera
 
@@ -103,7 +104,7 @@ n_pari_ok=n_mini_pari_min #si può scegliere anche n_mini_pari_max, per splittar
 
 if n_map% 2 == 0:
 ##########Splitting minimo (2 parti)
-    if n_pari_ok==n_mini_pari_min: 
+    if n_pari_ok==n_mini_pari_max: 
         # Dividi la matrice a metà
                
         #########Metà destra 
@@ -111,9 +112,8 @@ if n_map% 2 == 0:
         num_colonne = matrice.shape[1]
         meta_sinistra = matrice[:, :num_colonne // 2]
         ob_vec_1 = meta_sinistra.flatten()
-        P1=P
-        c1=c;
-        c1 = c1 / np.sum(c1)
+        P1=retaut_2(n_map)
+        c1=c
         for i in ob_vec_1:
             P1[i-1,:]=0
             P1[:,i-1]=0
@@ -133,12 +133,12 @@ if n_map% 2 == 0:
         
             
         ########Metà sinistra
-        P = retaut_2(n_map)
+        
         meta_destra = matrice[:, num_colonne // 2:]
         ob_vec_2 = meta_destra.flatten()
-        P2=P;
-        c2=c;
-        c2 = c2 / np.sum(c2)
+        P2=retaut_2(n_map)
+        c2=c
+        
         for i in ob_vec_2:
             P2[i-1,:]=0
             P2[:,i-1]=0
@@ -157,3 +157,90 @@ if n_map% 2 == 0:
         
     else: 
     ########Splitting massimo (4 parti)
+       ##Quarto in alto a sinistra (Quarto 1)
+        num_colonne = matrice.shape[1]
+        num_righe = matrice.shape[0]
+        quarto_1 = matrice[num_righe // 2:, num_colonne // 2:]
+        ob_vec_1 = quarto_1.flatten()
+        
+        P1= retaut_2(n_map)
+        c1=c
+        
+        for i in ob_vec_1:
+           P1[i-1,:]=0
+           P1[:,i-1]=0
+           c1[i-1]=0 
+        a = 1
+        b= int(N/2)-int(n_map/2)      
+        initial_state_1 = np.zeros(N)
+        initial_state_1[a-1] = 1
+        final_state_1 = np.zeros(N)
+        final_state_1[b-1] = 1 
+        maxPostProg_seq_c1=optimumPath(P1, initial_state_1,final_state_1, T, c1)
+        print(f"maxPostProg_seq_c1: {maxPostProg_seq_c1}")
+        
+      ##Quarto alto a destra (Quarto 2)
+        quarto_2 = matrice[num_righe // 2:, :num_colonne // 2]
+        ob_vec_2 = quarto_2.flatten()
+        P2= retaut_2(n_map)
+        c2=c
+        
+        for i in ob_vec_2:
+           P2[i-1,:]=0
+           P2[:,i-1]=0
+           c2[i-1]=0 
+        #print(tabulate(P2, tablefmt="fancy_grid"))
+        a = (int(n_map/2))+1
+        b= int(N/2) 
+        
+        initial_state_2 = np.zeros(N)
+        initial_state_2[a-1] = 1
+        final_state_2 = np.zeros(N)
+        final_state_2[b-1] = 1 
+        maxPostProg_seq_c2=optimumPath(P2, initial_state_2,final_state_2, T, c2)
+        print(f"maxPostProg_seq_c2: {maxPostProg_seq_c2}")
+
+                
+      ##Quarto basso a sinistra (Quarto 3)
+      
+        quarto_3 = matrice[:num_righe // 2, num_colonne // 2:]   
+        ob_vec_3 = quarto_3.flatten()
+        P3= retaut_2(n_map)
+        c3=c
+        
+        for i in ob_vec_3:
+           P3[i-1,:]=0
+           P3[:,i-1]=0
+           c3[i-1]=0 
+        #print(tabulate(P2, tablefmt="fancy_grid"))
+        a = int(N/2)+1
+        b= N-int(n_map/2) 
+        
+        initial_state_3 = np.zeros(N)
+        initial_state_3[a-1] = 1
+        final_state_3 = np.zeros(N)
+        final_state_3[b-1] = 1 
+        maxPostProg_seq_c3=optimumPath(P3, initial_state_3,final_state_3, T, c3)
+        print(f"maxPostProg_seq_c3: {maxPostProg_seq_c3}")        
+        
+        
+      ##Quarto basso a destra (Quarto 4)
+        quarto_4 = matrice[:num_righe // 2, :num_colonne // 2]   
+        ob_vec_4 = quarto_4.flatten()              
+        P4= retaut_2(n_map)
+        c4=c
+        
+        for i in ob_vec_4:
+           P4[i-1,:]=0
+           P4[:,i-1]=0
+           c4[i-1]=0 
+        #print(tabulate(P2, tablefmt="fancy_grid"))
+        a = int(N/2)+int(n_map/2)+1
+        b= N
+        
+        initial_state_4 = np.zeros(N)
+        initial_state_4[a-1] = 1
+        final_state_4 = np.zeros(N)
+        final_state_4[b-1] = 1 
+        maxPostProg_seq_c4=optimumPath(P4, initial_state_4,final_state_4, T, c4)
+        print(f"maxPostProg_seq_c4: {maxPostProg_seq_c4}")       
